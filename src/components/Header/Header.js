8d7bddb1b7713f53538a539466351bf2dd1bcb19
Sohazur <mdsohazurislam@gmail.com>
2022-01-29 19:06:17 +0400

    Added bootstrap

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1>MONEY HEIST</h1>
+      <h1>LA CASA DE PAPEL</h1>
       <h4>THE FIRST INTERNATIONAL HEIST</h4>
       <h2>TOTAL MONEY: ONE BILLION</h2>
     </div>
