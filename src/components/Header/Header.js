ff354ad34b3225c2fa531ac6b5196a2a4f34319f
Sohazur <mdsohazurislam@gmail.com>
2022-01-29 19:02:32 +0400

    Added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>MONEY HEIST</h1>
+      <h4>THE FIRST INTERNATIONAL HEIST</h4>
+      <h2>TOTAL MONEY: ONE BILLION</h2>
+    </div>
+  );
+};
+
+export default Header;
