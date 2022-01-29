763e6a9cbd7cb49b22570c2f21d287f1bd727f4a
Sohazur <mdsohazurislam@gmail.com>
2022-01-29 19:10:57 +0400

    Completed the header with styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <div className="header">
       <h1>LA CASA DE PAPEL</h1>
+      <hr />
       <h4>THE FIRST INTERNATIONAL HEIST</h4>
+      <hr />
       <h2>TOTAL MONEY: ONE BILLION</h2>
     </div>
   );
