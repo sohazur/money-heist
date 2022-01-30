a0a1bb090d2b9ac1ae482eadccfc4ab44010a662
Sohazur <mdsohazurislam@gmail.com>
2022-01-30 05:16:57 +0400

    Added total Price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,19 +2,17 @@ import React from "react";
 import "./Cart.css";
 const Cart = (props) => {
   const { cart } = props;
-  console.log(cart);
-  let selectedCharacters = [];
+  const characterCount = cart.length;
+  let totalPrice = 0;
   for (const character of cart) {
-    if (character) {
-      selectedCharacters.push(character);
-    }
+    totalPrice += character.price;
   }
-  console.log(selectedCharacters);
   return (
     <div>
-      <h1>Selected Player(s)</h1>
+      <h1>Player(s) Selected: {characterCount}</h1>
+      <h4>Total Price: ${totalPrice}</h4>
       <ol>
-        {selectedCharacters.map((character) => (
+        {cart.map((character) => (
           <li key={character.id * Math.random()}>{character.aliasName}</li>
         ))}
       </ol>
